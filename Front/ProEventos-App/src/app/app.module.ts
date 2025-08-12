fb0f80dac58102876e19fc5039480d5c29cda3f5
gabriel santos <gabriel89toca@gmail.com>
2025-08-12 18:48:33 -0300

    layout e binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './Palestrantes/Palestrantes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NavComponent } from './nav/nav.component';
 import { CollapseDirective, CollapseModule } from 'ngx-bootstrap/collapse';
+
 
 
 @NgModule({
@@ -23,7 +26,8 @@ import { CollapseDirective, CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   widthImg: number = 100;
   marginImg:  number = 2;
-
   exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  
+  public set filtroLista(value: string) {
+     this._filtroLista = value;
+     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || 
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -32,7 +51,10 @@ export class EventosComponent implements OnInit {
   public GetEventos(): void {
 
  this.http.get('https://localhost:7128/api/Eventos').subscribe(
-      Response => this.eventos = Response,
+      Response => {
+        this.eventos = Response;
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     ); 
 
