eb7921832f3a910f8bdea89c7b9d02429b27cd86
gabriel santos <gabriel89toca@gmail.com>
2025-10-31 18:59:38 -0300

    Cascate

---
+++ Front/ProEventos-App/model/Lote.ts
@@ -0,0 +1,10 @@
+export interface Lote {
+         Id: number;
+         Nome: string;
+         Preco: number;
+         DataInicio?: Date;
+         DataFim?: Date;
+         Quantidade: number;
+         EventoId: number;
+         Evento?: Evento[];
+}
